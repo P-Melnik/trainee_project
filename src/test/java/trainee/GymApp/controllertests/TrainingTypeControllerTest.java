e234b9921ec0a48929454a7f285a5c7a5c2c71a9
Pavel Melnik <pavel_melnik@epam.com>
2024-03-08 20:24:32 +0400

    boot controller tests ref

--- src/test/java/trainee/GymApp/controllertests/TrainingTypeControllerTest.java
+++ src/test/java/trainee/GymApp/controllertests/TrainingTypeControllerTest.java
@@ -1,13 +1,18 @@
 package trainee.GymApp.controllertests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import trainee.GymApp.facade.Facade;
 import trainee.GymApp.controllers.TrainingTypeController;
 import trainee.GymApp.entity.TrainingType;
@@ -15,35 +20,50 @@ import trainee.GymApp.entity.TrainingType;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
 public class TrainingTypeControllerTest {
 
-    @InjectMocks
-    private TrainingTypeController trainingTypeController;
+    private static final Set<TrainingType> typeSet = new HashSet<>();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    private MockMvc mvc;
 
     @Mock
     private Facade facade;
 
+    @InjectMocks
+    private TrainingTypeController trainingTypeController;
+
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        mvc = MockMvcBuilders.standaloneSetup(trainingTypeController).build();
     }
-
-    Set<TrainingType> set = new HashSet<>();
-    TrainingType trainingType1 = new TrainingType(1, "GYM");
-    TrainingType trainingType2 = new TrainingType(2, "RUNNING");
 
     @Test
-    void get() {
-        set.add(trainingType1);
-        set.add(trainingType2);
-        Mockito.doReturn(set).when(this.facade).getAllTrainingTypes();
-        Set<TrainingType> responseSet = trainingTypeController.get();
-        assertTrue(responseSet.containsAll(set));
-        verify(facade, times(1)).getAllTrainingTypes();
+    public void testGetTrainingTypes() throws Exception {
+        typeSet.add(new TrainingType(1, "GYM"));
+        typeSet.add(new TrainingType(2, "SWIMMING"));
+        Mockito.when(this.facade.getAllTrainingTypes()).thenReturn(typeSet);
+        mvc.perform(MockMvcRequestBuilders.get("/training-types")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].trainingTypeName").value("GYM"))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].trainingTypeName").value("SWIMMING"));
+    }
+
+    private String asJsonString(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
