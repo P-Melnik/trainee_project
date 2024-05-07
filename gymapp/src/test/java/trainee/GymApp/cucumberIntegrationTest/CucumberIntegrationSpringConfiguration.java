970d01b29a48cb34612ff33af95386716ca29797
Pavel Melnik <pavel_melnik@epam.com>
2024-05-07 19:31:33 +0400

    ref config

--- gymapp/src/test/java/trainee/GymApp/cucumberIntegrationTest/CucumberIntegrationSpringConfiguration.java
+++ gymapp/src/test/java/trainee/GymApp/cucumberIntegrationTest/CucumberIntegrationSpringConfiguration.java
@@ -1,13 +1,13 @@
-//package trainee.GymApp.cucumberIntegrationTest;
-//
-//import io.cucumber.spring.CucumberContextConfiguration;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.annotation.DirtiesContext;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//@CucumberContextConfiguration
-//@ActiveProfiles("testreport")
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//@SpringBootTest
-//public class CucumberIntegrationSpringConfiguration {
-//}
+package trainee.GymApp.cucumberIntegrationTest;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@CucumberContextConfiguration
+@ActiveProfiles("testreport")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest
+public class CucumberIntegrationSpringConfiguration {
+}
